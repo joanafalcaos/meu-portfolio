445978a698a6f0176a09e0737cf5e211920865fb
Joana <joanamfalcaos@gmail.com>
2024-10-28 11:24:07 -0300

    Ajustando o texto e imagem do sobre

--- src/componentes/Sobre.js
+++ src/componentes/Sobre.js
@@ -11,6 +11,7 @@ const Sobre = () => {
         <div className="sobre-texto">
           <h1>Joana Falcão</h1>
           <p>Estudante de Sistemas para Internet, Designer de produtos e apaixonada por UX/UI.</p>
+          <p> Iniciei minha trajetória no mundo da tecnológia em 2023, e desde então sigo apaixonada pela área, em especial pela esféra do Design. Tenho também experiência com desenvolvimento de aplicações com o Framework Flutter e criação de sites responsivos com HTML, CSS, JavaScript e React.</p>
         </div>
       </div>
     </section>
